5346adac2c25dfcf1011b02107d0ca01e74364e1
ctpolimdo <indriantara9@gmail.com>
2020-03-17 05:24:13 -0700

    Connecting Time

---
+++ assets/library/js/scroolSmoth.js
@@ -0,0 +1,18 @@
+var posY = 0;
+var jarak = 20;
+
+function smoothScroll(id){
+	var target = document.getElementById(id).offsetTop;
+	var scrollAnimate = setTimeout(function(){
+		smoothScroll(id)
+	}, 10);
+
+	posY = posY + jarak;
+
+	if (posY >= target) {
+		clearTimeout(scrollAnimate);
+		posY = 0;
+	}else{
+		window.scroll(0, posY);
+	}
+}
